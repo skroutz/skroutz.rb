dbc762df84d2abb0e939d016a09dfce451577e7b
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-11 14:18:13 +0200

    Make SkroutzApi::Resource behave like ActiveModel

--- lib/skroutz_api/resource.rb
+++ lib/skroutz_api/resource.rb
@@ -1,9 +1,10 @@
 class SkroutzApi::Resource
   include SkroutzApi::Parsing
 
-  attr_accessor :client
+  attr_accessor :attributes, :client
 
-  def initialize(client)
+  def initialize(attributes, client)
+    @attributes = attributes
     @client = client
   end
 
@@ -34,5 +35,34 @@ class SkroutzApi::Resource
 
   def resource_prefix
     @resource_prefix ||= self.class.to_s.demodulize.downcase.pluralize
+  end
+
+  protected
+
+  def respond_to?(method, include_priv = false)
+    method_name = method.to_s
+    if attributes.nil?
+      super
+    elsif attributes.include?(method_name.sub(/[=\?]\Z/, ''))
+      true
+    else
+      super
+    end
+  end
+
+  def method_missing(method_symbol, *arguments)
+    method_name = method_symbol.to_s
+
+    if method_name =~ /(=|\?)$/
+      case $1
+      when '='
+        attributes[$`] = arguments.first
+      when '?'
+        attributes[$`]
+      end
+    else
+      return attributes[method_name] if attributes.include?(method_name)
+      super
+    end
   end
 end
