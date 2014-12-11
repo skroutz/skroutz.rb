286e164770d1f720fe20870110ec5c0945749a94
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-11 14:08:22 +0200

    Make SkroutzApi::Parsing parse methods return SkroutzApi::Resource instances instead of Openstructs

--- lib/skroutz_api/parsing.rb
+++ lib/skroutz_api/parsing.rb
@@ -1,11 +1,11 @@
 module SkroutzApi::Parsing
   def parse_body(response)
-    OpenStruct.new((JSON.parse response.body)[resource_prefix.singularize])
+    model_name.new((JSON.parse response.body)[resource_prefix.singularize], client)
   end
 
   def parse_array(response, resource_prefix)
     (JSON.parse response.body)[resource_prefix].map do |resource|
-      OpenStruct.new(resource)
+      model_name.new(resource, client)
     end
   end
 
