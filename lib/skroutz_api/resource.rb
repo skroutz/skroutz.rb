6e2dc69b37a14bf4861cf80de38a314f8a9f5969
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-11 14:19:08 +0200

    Remove CollectionProxy methods from Resource

--- lib/skroutz_api/resource.rb
+++ lib/skroutz_api/resource.rb
@@ -8,31 +8,6 @@ class SkroutzApi::Resource
     @client = client
   end
 
-  def find(id, options = {})
-    response = client.get("#{resource_prefix}/#{id}")
-
-    return parse_body(response) unless block_given?
-
-    yield response
-  end
-
-  def page(pagenum = 1, options = {})
-    per = options[:per] || client.config[:pagination_page_size]
-    response = client.get(resource_prefix, page: pagenum, per: per)
-
-    return SkroutzApi::PaginatedCollection.new(self, response) unless block_given?
-
-    yield response
-  end
-
-  def all(options = {})
-    response = client.get(resource_prefix)
-
-    return SkroutzApi::PaginatedCollection.new(self, response) unless block_given?
-
-    yield response
-  end
-
   def resource_prefix
     @resource_prefix ||= self.class.to_s.demodulize.downcase.pluralize
   end
