0c5dcf075d0dec90b995e48c636e776598bd5f7d
Fotos Georgiadis <gfotos@gmail.com>
2015-02-28 12:39:06 +0200

    Add SkroutzApi::ServerError exception

--- lib/skroutz_api/errors.rb
+++ lib/skroutz_api/errors.rb
@@ -5,6 +5,13 @@ module SkroutzApi
   class SkroutzApiError < StandardError; end
 
   # Raised when SkroutzApi handles a non-successful response
+  class ServerError < SkroutzApiError
+    def initialize(status, body)
+      super %Q(status: #{status}, body: "#{body}")
+    end
+  end
+
+  # Raised when SkroutzApi doesn't find the requested resource
   class ResourceNotFound < SkroutzApiError
     def initialize(response)
       super %Q(status: #{response.status}, body: "#{response.body}")
