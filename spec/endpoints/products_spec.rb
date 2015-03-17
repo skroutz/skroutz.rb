cafdd222eb1f98e9694524ba3a6d04e1e19ffcf1
Dimitris Zorbas <zorbash@skroutz.gr>
2015-03-17 23:08:51 +0200

    Add products endpoints spec

---
+++ spec/endpoints/products_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'products' do
+  include_context 'resource'
+
+  subject(:products) { client.products }
+
+  it_behaves_like 'a resource', only: [:find] do
+    let(:resource) { products }
+  end
+end
