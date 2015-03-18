50027f4086a118b5be7daf86b629d78e0d6d057b
Dimitris Zorbas <zorbash@skroutz.gr>
2015-03-18 10:42:38 +0200

    Add SkroutzApi::Parsing#parse and infer resources and collections based on root key

--- lib/skroutz_api/parsing.rb
+++ lib/skroutz_api/parsing.rb
@@ -1,24 +1,54 @@
 module SkroutzApi::Parsing
-  def parse_body(response)
-    model_name.new((JSON.parse response.body)[resource_prefix.singularize], client)
-  rescue JSON::ParserError
-    raise SkroutzApi::InvalidResource.new(model_name.to_s, response.body)
-  end
+  def parse(response)
+    json = JSON.parse(response.body)
 
-  def parse_array(response, resource_prefix)
-    (JSON.parse response.body)[resource_prefix].map do |resource|
-      model_name.new(resource, client)
+    if collection?(json)
+      SkroutzApi::PaginatedCollection.new(self, response, parse_collection(json))
+    else
+      parse_resource(json)
     end
   rescue JSON::ParserError
     raise SkroutzApi::InvalidResource.new(model_name.to_s, response.body)
   end
 
+  def parse_resource(json)
+    infer_model(json).new(json[resource_key(json)], client)
+  end
+
+  def parse_collection(json)
+    model_name = infer_collection(json)
+
+    json[collection_resource_key(json).pluralize].map do |resource|
+      model_name.new(resource, client)
+    end
+  end
+
   def link_header(response)
-    response.headers['link'].split(',').reduce({}) do |h, link|
+    response.headers.fetch('link', '').split(',').reduce({}) do |h, link|
       page, type = link.scan(/\<(.*)\>; rel="(.*)"/).flatten
       h[type.to_sym] = page
 
       h
     end
+  end
+
+  def resource_key(json)
+    json.keys.first
+  end
+
+  def collection_resource_key(json)
+    (json.keys - ['meta']).first.singularize
+  end
+
+  def infer_model(json)
+    "SkroutzApi::#{resource_key(json).capitalize}".safe_constantize
+  end
+
+  def infer_collection(json)
+    "SkroutzApi::#{collection_resource_key(json).capitalize}".safe_constantize
+  end
+
+  def collection?(json)
+    json[(json.keys - ['meta']).first].is_a?(Array)
   end
 end
